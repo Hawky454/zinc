209818ed60cb3fe09025eca857cfddf8dafa29a2
David Miller <hawky454@gmail.com>
2018-08-03 11:27:06 -0500

    First commit for first part of the assignment

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -4,40 +4,25 @@
 
 const Zinc = {};
 
-const userData = {
-    picture: {
-        thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
-    },
-    name: {
-        first: 'Jack',
-        last: 'Burton'
-    },
-    location: {
-        city: 'San Francisco',
-        state: 'CA'
-    },
-    email: 'jack.burton@example.com'
-};
 
 (() => {
-    function renderComponent(element, content, data) {
-        let userItem = document.querySelector(element);
-        fetch(`${content}.html`)
-        .then(content => content.text())
-        .then((content) => {
-            let regEx = /{{\s*([\w.]+)\s*}}/g; 
-            let HTML=content.replace(regEx, (match, templateValue) => {
-                console.log(templateValue);
-                let templateValueArr = templateValue.split('.');
-                return templateValueArr.reduce((acc ,curr) => acc[curr], data)
-                })
-            userItem.insertAdjacentHTML('beforeend', HTML);
+    function renderComponent(element, content, userData) {
+
+        fetch(`user.html`)
+        .then(template => template.text())
+        .then(template => {
+            let matchString = /{{\s*([\w.]+)\s*}}/g;
+
+            let user = template.replace(matchString, (match, captured) => {
+                let arr = captured.split('.');
+                return arr.reduce((acc, curr) => acc[curr], userData);
             })
-        // console.log(element, content); // eslint-disable-line no-console
+            document.getElementsByTagName('user-item')[0].insertAdjacentHTML('beforeend', user);
+        });
     }
 
     function init() {
-        renderComponent('user-item', 'user', userData);
+        renderComponent('user-item', 'user', Zinc.userData);
     }
 
     document.addEventListener('DOMContentLoaded', init);
