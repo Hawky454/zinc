5bd996e10677aa3aa0baa92c41a3f317b9ea1539
David Miller <hawky454@gmail.com>
2018-08-06 11:55:49 -0500

    Still working on part 3 but breaking for lunch

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,17 +1,64 @@
 'use strict';
-
 /* eslint-env browser */
-
 const Zinc = {};
-
 (() => {
-    function renderComponent(element, content) {
-        console.log(element, content); // eslint-disable-line no-console
+  function hilite() {
+    element.addEventListener('click', e => {
+      e.currentTarget.classList.toggle('hilight');
+      console.log(true);
+    });
+  };
+  function renderComponent(element, content, data, controller) {
+    let regex = /{{\s*([\w.]+)\s*}}/g;
+    let elements = Array.from(document.getElementsByTagName(element));
+    fetch(`${content}.html`)
+      .then(content => content.text())
+      .then((content) => {
+        elements.forEach(element => {
+          element.addEventListener('click', controller);
+          let HTML = content.replace(regex, (match, templateValue) => {
+            let templateValueArr = templateValue.split('.');
+            return templateValueArr.reduce((acc, curr) => acc[curr], data)
+          })
+          element.insertAdjacentHTML('beforeend', HTML);
+        })
+      })
     }
-
+    // passing components = Zinc.components
+    function renderComponents(components) {
+      // looping through the keys and values of components object
+      for(let component in components) {
+        //passing content in object to function to render components (put content in html)
+        renderComponent(
+          components[component].elementName,
+          components[component].templateFile,
+          components[component].dataObject,
+          components[component].controller)
+      }
+    }
+    Zinc.registerComponent = function(elementName, templateFile, dataObject, controller) {
+        if (!Zinc.components) {
+          Zinc.components = {};
+        }
+        Zinc.components[elementName] = {
+          elementName,
+          templateFile,
+          dataObject,
+          controller
+        }
+    }
     function init() {
-        renderComponent('user-item', 'user', userData);
+      //removes Jack form the list
+      // Zinc.registerComponent('user-item', 'user', Zinc.userData, controller);
+      // renderComponents(Zinc.components);
+      fetch('https://randomuser.me/api/?results=7')
+      .then(res => res.json())
+      .then(data => {
+        data.results.forEach(user => {
+          Zinc.registerComponent('user-item', 'user', user, hilite);
+          renderComponents(Zinc.components);
+        })
+      })
     }
-
     document.addEventListener('DOMContentLoaded', init);
 })();
